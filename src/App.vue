c038565f802b692c67c3763e751a58c0589d06a4
Angga Jiyan <anggajiyanfajar@gmail.com>
2023-09-13 06:54:03 +0700

    Add Loading component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div v-if="appReady" class="min-h-full font-Poppins box-border">
     <Navigation />
-    <router-view />
+    <router-view /> 
+  </div>
+  <div v-if="!appReady" class="h-screen flex flex-col justify-evenly items-center">
+    <Loading />
+    <p>Active Tracker</p>
   </div>
 </template>
 
 <script>
 import Navigation from './components/Navigation.vue'
+import Loading from './components/Loading.vue'
 import {ref} from 'vue'
 import {supabase} from './supabase/init'
 import store from './store/index'
@@ -14,10 +19,11 @@ import store from './store/index'
 export default {
   components: {
     Navigation,
+    Loading
   },
   setup() {
     // Create data / vars
-    const appReady = ref(null)
+    const appReady = ref(false)
     // Check to see if user is already logged in
     const checkUser = async() => {
       const { data: { user } } = await supabase.auth.getUser()
@@ -27,13 +33,20 @@ export default {
     
     // If user does not exist, need to make app ready
     if (!user){
-      appReady.value = true
+      appReady.value = false
+      setTimeout(() => {
+        appReady.value = true
+      }, 2000);
+
     }
     // Runs when there is a auth state change
     // if user is logged in, this will fire
     supabase.auth.onAuthStateChange((_, session) => {
       store.methods.setUser(session);
-      appReady.value = true
+      appReady.value = false
+      setTimeout(() => {
+        appReady.value = true
+      }, 2000);
     })
     return {appReady};
   },
